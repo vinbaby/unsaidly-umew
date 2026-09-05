89e885e4d3d2f16f56cc64baa0ce3aaf05d63083
vinbaby <81433984+vinbaby@users.noreply.github.com>
2026-09-05 14:47:56 +0700

    Delete unsaidlyversion2 directory

--- unsaidlyversion2/supabase_v10_thumbnail.sql
+++
@@ -1,2 +0,0 @@
--- Pigpic V10: thumbnail key for fast feed loading
-alter table posts add column if not exists thumb_key text;
--- unsaidlyversion2/supabase_v16_profile_media.sql
+++
@@ -1,2 +0,0 @@
-alter table profiles add column if not exists cover_url text;
-create index if not exists profiles_username_idx on profiles(username);
